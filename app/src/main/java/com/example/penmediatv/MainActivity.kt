17675e5f4aa6b1f8e2d3a7af82848d06e96cd215
wendy <1390369717@qq.com>
2024-08-05 22:46:48 +0800

    初版代码

---
+++ app/src/main/java/com/example/penmediatv/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.penmediatv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.penmediatv.databinding.ActivityMainBinding
+import com.example.penmediatv.ui.theme.PenmediaTVTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
