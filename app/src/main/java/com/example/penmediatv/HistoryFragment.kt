adc6234f5875cae11e5f301de3899e1ab64c3526
wendy <1390369717@qq.com>
2024-08-08 19:59:42 +0800

    feat(HomeFragment):movie_item添加边框未成功

---
+++ app/src/main/java/com/example/penmediatv/HistoryFragment.kt
@@ -0,0 +1,56 @@
+package com.example.penmediatv
+
+import android.animation.AnimatorInflater
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.penmediatv.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        binding.firstFocusableElement.requestFocus()
+        binding.firstFocusableElement.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                //显示标题和副标题
+                binding.title.visibility = View.VISIBLE
+                binding.subtitle.visibility = View.VISIBLE
+
+                //放大效果
+                v.animate().setDuration(200).scaleX(1.1f).scaleY(1.1f).start()
+                v.stateListAnimator =
+                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_up)
+            } else {
+                //隐藏标题和副标题
+                binding.title.visibility = View.GONE
+                binding.subtitle.visibility = View.GONE
+
+                //恢复原始大小
+                v.animate().setDuration(200).scaleX(1.0f).scaleY(1.0f).start()
+                v.stateListAnimator =
+                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_down)
+            }
+        }
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //确保在Fragment可见时再请求焦点
+        binding.firstFocusableElement.requestFocus()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
