a3c40bd3dd190ae2b4eeb0b09729ba86d092c0ed
wendy <1390369717@qq.com>
2024-08-20 18:31:23 +0800

    feat(fragment_history):item_history绘制，解决仅出现三项的问题

--- app/src/main/java/com/example/penmediatv/HistoryFragment.kt
+++ app/src/main/java/com/example/penmediatv/HistoryFragment.kt
@@ -22,9 +22,9 @@ class HistoryFragment : Fragment() {
         binding.recyclerView.layoutManager = GridLayoutManager(context,3)
         val movieList = listOf(
             Movie("Movie 1", R.drawable.movie),
-            Movie("Movie 2", R.drawable.ic_search),
-            Movie("Movie 3", R.drawable.ic_history),
-            Movie("Movie 4", R.drawable.ic_mine),
+            Movie("Movie 2", R.drawable.movie),
+            Movie("Movie 3", R.drawable.movie),
+            Movie("Movie 4", R.drawable.movie),
             Movie("Movie 5", R.drawable.ic_search),
             Movie("Movie 6", R.drawable.ic_history),
         )
