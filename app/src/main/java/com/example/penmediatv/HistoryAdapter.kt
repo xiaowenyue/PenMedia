5563ea08f173e5314f63545e6cae03a4e7f23096
wendy <1390369717@qq.com>
2024-08-20 19:03:55 +0800

    feat(fragment_history):聚焦变化未解决

--- app/src/main/java/com/example/penmediatv/HistoryAdapter.kt
+++ app/src/main/java/com/example/penmediatv/HistoryAdapter.kt
@@ -2,9 +2,9 @@ package com.example.penmediatv
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.penmediatv.databinding.ItemHistoryBinding
-import com.example.penmediatv.databinding.ItemMovieBinding
 
 class HistoryAdapter(private val movies: List<Movie>) :
     RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
@@ -13,10 +13,21 @@ class HistoryAdapter(private val movies: List<Movie>) :
         fun bind(movie: Movie) {
             binding.title.text = movie.name
             binding.pic.setImageResource(movie.imageResId)
-            binding.play.setOnClickListener {
+            binding.item.setOnClickListener{}
+            binding.item.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    Toast.makeText(
+                        binding.root.context,
+                        "Focused on ${movie.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    println("no focus")
+                }
             }
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
         val binding = ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HistoryViewHolder(binding)
