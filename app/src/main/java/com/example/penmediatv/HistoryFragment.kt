a8166367e822cc2465c6b502a86ce93e17d272b9
wendy <1390369717@qq.com>
2024-08-20 16:48:37 +0800

    feat(fragment_history):观看历史页面UI开始绘制

--- app/src/main/java/com/example/penmediatv/HistoryFragment.kt
+++ app/src/main/java/com/example/penmediatv/HistoryFragment.kt
@@ -1,11 +1,11 @@
 package com.example.penmediatv
 
-import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.penmediatv.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
@@ -13,55 +13,22 @@ class HistoryFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
-        binding.firstFocusableElement.setOnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                //显示标题和副标题
-                binding.title.visibility = View.VISIBLE
-                binding.subtitle.visibility = View.VISIBLE
 
-                //放大效果
-                v.animate().setDuration(200).scaleX(1.1f).scaleY(1.1f).start()
-                v.stateListAnimator =
-                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_up)
-            } else {
-                //隐藏标题和副标题
-                binding.title.visibility = View.GONE
-                binding.subtitle.visibility = View.GONE
-
-                //恢复原始大小
-                v.animate().setDuration(200).scaleX(1.0f).scaleY(1.0f).start()
-                v.stateListAnimator =
-                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_down)
-            }
-        }
-        binding.firstFocusableElement.setOnClickListener {}
-
-        binding.firstFocusableElement0.setOnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                //显示标题和副标题
-                binding.title0.visibility = View.VISIBLE
-                binding.subtitle0.visibility = View.VISIBLE
-
-                //放大效果
-                v.animate().setDuration(200).scaleX(1.1f).scaleY(1.1f).start()
-                v.stateListAnimator =
-                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_up)
-            } else {
-                //隐藏标题和副标题
-                binding.title0.visibility = View.GONE
-                binding.subtitle0.visibility = View.GONE
-
-                //恢复原始大小
-                v.animate().setDuration(200).scaleX(1.0f).scaleY(1.0f).start()
-                v.stateListAnimator =
-                    AnimatorInflater.loadStateListAnimator(context, R.animator.scale_down)
-            }
-        }
-        binding.firstFocusableElement0.setOnClickListener {}
+        binding.recyclerView.layoutManager = GridLayoutManager(context,3)
+        val movieList = listOf(
+            Movie("Movie 1", R.drawable.movie),
+            Movie("Movie 2", R.drawable.ic_search),
+            Movie("Movie 3", R.drawable.ic_history),
+            Movie("Movie 4", R.drawable.ic_mine),
+            Movie("Movie 5", R.drawable.ic_search),
+            Movie("Movie 6", R.drawable.ic_history),
+        )
+        binding.recyclerView.adapter = MovieAdapter(movieList)
 
         return binding.root
     }
