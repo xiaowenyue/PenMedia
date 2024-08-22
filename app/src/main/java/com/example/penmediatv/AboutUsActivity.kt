3f86c4384a4306c5035069be316872b907b94c99
wendy <1390369717@qq.com>
2024-08-22 18:02:57 +0800

    feat(activity_about_us):关于我们点击事件焦点处理功能开发失败

--- app/src/main/java/com/example/penmediatv/AboutUsActivity.kt
+++ app/src/main/java/com/example/penmediatv/AboutUsActivity.kt
@@ -2,10 +2,43 @@ package com.example.penmediatv
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.penmediatv.databinding.ActivityAboutUsBinding
 
 class AboutUsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_us)
+        binding = ActivityAboutUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.company.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button_pressed))
+            } else {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button))
+            }
+        }
+        binding.software.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button_pressed))
+            } else {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button))
+            }
+        }
+        binding.version.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button_pressed))
+            } else {
+                binding.viewCompany.setBackgroundColor(resources.getColor(R.color.button))
+            }
+        }
+        binding.company.setOnClickListener {
+            binding.company.setBackgroundColor(resources.getColor(R.color.button_pressed))
+        }
+        binding.software.setOnClickListener {
+            binding.software.setBackgroundColor(resources.getColor(R.color.button_pressed))
+        }
+        binding.version.setOnClickListener {
+            binding.version.setBackgroundColor(resources.getColor(R.color.button_pressed))
+        }
     }
 }
