aab96d9c08a66b2b1c512e4b571d62cdb30b97d4
wendy <1390369717@qq.com>
2024-08-26 16:06:55 +0800

    feat(activity_movie_details):watch now按钮点击事件实现

--- app/src/main/java/com/example/penmediatv/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/penmediatv/MovieDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.penmediatv
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.penmediatv.databinding.ActivityMovieDetailsBinding
 
@@ -10,5 +11,9 @@ class MovieDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btWatchNow.setOnClickListener {
+            Toast.makeText(this, "Watch Now", Toast.LENGTH_SHORT).show()
+        }
     }
 }
