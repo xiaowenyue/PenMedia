a4216b1e2d52a0bd93fcf6609e772b95335346f9
wendy <1390369717@qq.com>
2024-08-29 17:04:26 +0800

    feat(MoviePlayActivity):腾讯云视立方播放器SDK通过url启动播放

--- app/src/main/java/com/example/penmediatv/MoviePlayActivity.kt
+++ app/src/main/java/com/example/penmediatv/MoviePlayActivity.kt
@@ -1,6 +1,7 @@
 package com.example.penmediatv
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.penmediatv.databinding.ActivityMoviePlayBinding
@@ -50,18 +51,18 @@ class MoviePlayActivity : AppCompatActivity() {
 
         //播放 URL 视频资源
 //        val url = "https://cdn.coverr.co/videos/coverr-serene-surfer-gazing-at-the-sea/720p.mp4"
-//        val url = "http://vjs.zencdn.net/v/oceans.mp4"
-//        mVodPlayer.startVodPlay(url)
+        val url = "http://vjs.zencdn.net/v/oceans.mp4"
+        mVodPlayer.startVodPlay(url)
 
 
         // 推荐使用下面的新接口
 // psign 即播放器签名，签名介绍和生成方式参见链接：https://cloud.tencent.com/document/product/266/42436
-        val playInfoParam = TXPlayInfoParams(
-            1329031633,  // 腾讯云账户的appId
-            "1397757891767785731",  // 视频的fileId
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6MTMyOTAzMTYzMywiZmlsZUlkIjoiMTM5Nzc1Nzg5MTc2Nzc4NTczMSIsImN1cnJlbnRUaW1lU3RhbXAiOjE3MjQ5MDIzOTIsImNvbnRlbnRJbmZvIjp7ImF1ZGlvVmlkZW9UeXBlIjoiT3JpZ2luYWwifSwidXJsQWNjZXNzSW5mbyI6eyJkb21haW4iOiIxMzI5MDMxNjMzLnZvZC1xY2xvdWQuY29tIiwic2NoZW1lIjoiSFRUUFMifX0.DvysgSk3OTKyZSZE8grg8ywnQObl0FHrGtRbtvvlVTk"
-        ) // 播放器签名
-        mVodPlayer.startVodPlay(playInfoParam)
+//        val playInfoParam = TXPlayInfoParams(
+//            1329031633,  // 腾讯云账户的appId
+//            "1397757891767785731",  // 视频的fileId
+//            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6MTMyOTAzMTYzMywiZmlsZUlkIjoiMTM5Nzc1Nzg5MTc2Nzc4NTczMSIsImN1cnJlbnRUaW1lU3RhbXAiOjE3MjQ5MDIzOTIsImNvbnRlbnRJbmZvIjp7ImF1ZGlvVmlkZW9UeXBlIjoiT3JpZ2luYWwifSwidXJsQWNjZXNzSW5mbyI6eyJkb21haW4iOiIxMzI5MDMxNjMzLnZvZC1xY2xvdWQuY29tIiwic2NoZW1lIjoiSFRUUFMifX0.DvysgSk3OTKyZSZE8grg8ywnQObl0FHrGtRbtvvlVTk"
+//        ) // 播放器签名
+//        mVodPlayer.startVodPlay(playInfoParam)
     }
 
     public override fun onDestroy() {
