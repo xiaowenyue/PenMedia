6d7c3e072e2c6503abdf89bd009cd51a7979c591
wendy <1390369717@qq.com>
2024-09-04 10:32:34 +0800

    feat(fragment_search):片名列表绘制

--- app/src/main/java/com/example/penmediatv/PopularMoviesAdapter.kt
+++ app/src/main/java/com/example/penmediatv/PopularMoviesAdapter.kt
@@ -8,19 +8,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.penmediatv.databinding.ItemMovieBinding
+import com.example.penmediatv.databinding.ItemSearchMovieBinding
 
 class PopularMoviesAdapter(private val movies: List<Movie>) :
     RecyclerView.Adapter<PopularMoviesAdapter.MovieViewHolder>() {
 
-    inner class MovieViewHolder(val binding: ItemMovieBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(movie: Movie){
-            binding.movieTitle.text = movie.name
-            binding.movieImage.setImageResource(movie.imageResId)
+    inner class MovieViewHolder(val binding: ItemSearchMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            binding.btnMovieName.text = movie.name
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemSearchMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MovieViewHolder(binding)
     }
 
