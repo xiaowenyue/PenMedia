495c80c64ab531a69e624a3824371bbdc8ee8dd3
wendy <1390369717@qq.com>
2024-10-10 11:17:48 +0800

    feat(VideoPlayActivity)：使用bunny播放链接播放视频，出现SSL证书校验问题以及bunny自定义界面按钮无法操作问题

---
+++ app/src/main/java/com/example/penmediatv/VideoPlayActivity.kt
@@ -0,0 +1,39 @@
+package com.example.penmediatv
+
+import android.net.http.SslError
+import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.penmediatv.R
+
+class VideoPlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_play)
+
+        val videoWebView = findViewById<WebView>(R.id.videoWebView)
+        videoWebView.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler?,
+                error: SslError?
+            ) {
+                handler?.proceed()//忽略SSL错误
+            }
+        }
+        // 启用JavaScript
+        val webSettings: WebSettings = videoWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        // 使用WebViewClient确保WebView在当前应用中打开
+        videoWebView.webViewClient = WebViewClient()
+
+        // 这里加载你的Bunny视频链接
+        val videoUrl = "https://iframe.mediadelivery.net/play/314818/681efde2-1905-4405-9247-f0a87b3c8af7"
+        videoWebView.loadUrl(videoUrl)
+    }
+}
